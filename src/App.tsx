53b48f2b433c40ea2bf1cf1181d4e45ff3eaaf29
krishnaasikka99-spec <krishnaasikka99@gmail.com>
2025-10-22 21:03:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,3 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import Quality from './pages/Quality';
-import GlobalReach from './pages/GlobalReach';
-import Partner from './pages/Partner';
-import Contact from './pages/Contact';
-
-function App() {
-  return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navigation />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/quality" element={<Quality />} />
-            <Route path="/global-reach" element={<GlobalReach />} />
-            <Route path="/partner" element={<Partner />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+export default function App() {
+  return <h1>SpiceVista Exports Works!</h1>;
 }
-
-export default App;
