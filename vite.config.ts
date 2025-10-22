e004fd7c62ac5aff05e7758e971db0c10f471d86
krishnaasikka99-spec <krishnaasikka99@gmail.com>
2025-10-22 22:33:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/spicevistaexports/',
+  base: '/spicevistaexports/', // must match repo name
 });
